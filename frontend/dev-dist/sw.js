f3dcfbf88687252173fffdaea056abae39303191
Guilherme Simonato <guilhermesimonato4@gmail.com>
2026-02-18 23:13:15 -0300

    mongoosee

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8419cpopbig"
+    "revision": "0.fug6h6pl5q"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
